4acf7b0d9360d78de0057beee1dd1eac8153b04f
Olympior <145139878+Olympior@users.noreply.github.com>
2024-04-05 13:47:22 -0300

    Add files via upload

---
+++ ATV2/client.py
@@ -0,0 +1,22 @@
+import socket
+
+# Configurações do cliente
+HOST = '192.168.0.10'  # Endereço IP do servidor (localhost)
+PORT = 12345        # Porta para comunicação
+
+# Inicializa o socket do cliente
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+try:
+    nome = input("Digite seu nome: ")  # Solicitação do nome do cliente
+    client_socket.sendto(nome.encode('utf-8'), (HOST, PORT))  # Envio do nome ao servidor
+    
+    while True:
+        letra = input("Digite seu palpite: \n")
+        client_socket.sendto(letra.encode('utf-8'), (HOST, PORT))
+        resposta, _ = client_socket.recvfrom(1024)
+        print(resposta.decode('utf-8'))
+except KeyboardInterrupt:
+    print("Cliente encerrado.")
+finally:
+    client_socket.close()
