e6641e4adad0fb6d2dc06c8404ee5533a8ac3a3e
Olympior <victorolympio5@outlook.com.br>
2024-09-16 02:06:30 -0300

    SOAP

--- SOAP/server.py
+++ SOAP/server.py
@@ -3,19 +3,21 @@ from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
 from wsgiref.simple_server import make_server
 
-class NameService(ServiceBase):
+class ExampleService(ServiceBase):
     @rpc(Unicode, _returns=Unicode)
-    def save_name(ctx, name):
-        print(f"Nome recebido: {name}")
-        return f"O nome {name} foi salvo com sucesso."
+    def example_method(ctx, name):
+        return f"Name received: {name}"
 
-# Configurando a aplicação SOAP
-application = Application([NameService], 'urn:nome_service',
-                          in_protocol=Soap11(), out_protocol=Soap11())
+application = Application(
+    [ExampleService],
+    tns='http://example.org/wsdl/',
+    in_protocol=Soap11(),
+    out_protocol=Soap11()
+)
 
 wsgi_application = WsgiApplication(application)
 
 if __name__ == '__main__':
     server = make_server('localhost', 8000, wsgi_application)
-    print("Servidor SOAP está rodando em http://localhost:8000")
+    print("Server is running on http://localhost:8000/?wsdl")
     server.serve_forever()
